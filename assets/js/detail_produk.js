e8dce7d3678aaaab0adf875579c84ed4148f727d
chyntiadt-ship-it <chyntia.dt@gmail.com>
2026-05-27 16:01:45 +0700

    Delete assets directory

--- assets/js/detail_produk.js
+++
@@ -1,8 +0,0 @@
-const accordion = document.querySelector(".accordion");
-const accordionHeader = document.querySelector(".accordion-header");
-
-if (accordion && accordionHeader) {
-    accordionHeader.addEventListener("click", function () {
-        accordion.classList.toggle("active");
-    });
-}
